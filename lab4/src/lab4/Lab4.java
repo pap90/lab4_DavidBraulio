562e52d229e7d74995bf8225c3be830002300c56
pap90 <davidpejuan@unitec.edu>
2018-11-02 15:19:29 -0600

    actua

--- lab4/src/lab4/Lab4.java
+++ lab4/src/lab4/Lab4.java
@@ -7,6 +7,7 @@ package lab4;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import static lab4.pateador.sc;
 
 /**
  *
@@ -19,8 +20,8 @@ public class Lab4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ArrayList jugadores = new ArrayList();
-        ArrayList equipos = new ArrayList();
+        ArrayList<jugadores> jugadores = new ArrayList();
+        ArrayList<equipo> equipos = new ArrayList();
         Scanner sc = new Scanner(System.in);
         int resp = -1;
         while (resp != 0) {
@@ -31,7 +32,106 @@ public class Lab4 {
             resp = sc.nextInt();
             switch (resp) {
                 case 1:
+                    System.out.println("ingrese que desea hacer--"
+                            + "\n1) crear equipo"
+                            + "\n2) modificar equipo"
+                            + "\n3) eliminar equipo"
+                            + "\n4) listar ");
+                    int trabajo = sc.nextInt();
+                    switch (trabajo) {
+                        case 1:
+                            System.out.println("eligio crear");
+                            System.out.println("ingrese el nombre del equipo");
+                            String nombre = sc.next();
+                            System.out.println("ingrese el nombre del estadio");
+                            String estadio = sc.next();
+                            System.out.println("ingrese el pais del equipo");
+                            String pais = sc.next();
+                            System.out.println("ingrese el nombre del entrenador");
+                            String entrena = sc.next();
+                            System.out.println("ingrese el nombre del dueño del equipo");
+                            String dueño = sc.next();
+                            System.out.println("ingrese el nombre de la mascota");
+                            String mascota = sc.next();
 
+                            break;
+                        case 2:
+                            System.out.println("eligio modificar");
+                            for (int i = 0; i < equipos.size(); i++) {
+                                System.out.println(i + ")" + equipos.get(i).getNombre());
+                            }
+                            System.out.println("ingrese el numero del equipo que quiere modificar");
+                            int posi = sc.nextInt();
+                            System.out.println("que desea modificar?\n1)nombre"
+                                    + "\n2)estadio"
+                                    + "\n3)pais"
+                                    + "\n4)entrenador"
+                                    + "\n5)dueño"
+                                    + "\n6)mascota");
+                            int modi = sc.nextInt();
+                            switch (modi) {
+                                case 1:
+                                    System.out.println("modificar nombre");
+                                    System.out.println("ingrese el nuevo nombre del equipo");
+                                    String Nnombre = sc.next();
+                                    equipos.get(posi).setNombre(Nnombre);
+                                    break;
+                                case 2:
+                                    System.out.println("modificar estadio");
+                                    System.out.println("ingrese el nuevo nombre del estadio");
+                                    String est = sc.next();
+                                    equipos.get(posi).setEstadio(est);
+                                    break;
+                                case 3:
+                                    System.out.println("modificar pais");
+                                    System.out.println("ingrese el nuevo pais");
+                                    String Npais = sc.next();
+                                    equipos.get(posi).setPais(Npais);
+                                    break;
+                                case 4:
+                                    System.out.println("modificar entrenador");
+                                    System.out.println("ingrese el nuevo nombre del entrenador");
+                                    String entre = sc.next();
+                                    equipos.get(posi).setEntrenador(entre);
+                                    break;
+                                case 5:
+                                    System.out.println("modificar dueño");
+                                    System.out.println("ingrese el nuevo nombre del dueño");
+                                    String Duen = sc.next();
+                                    equipos.get(posi).setDueño(Duen);
+
+                                    break;
+                                case 6:
+                                    System.out.println("modificar mascota");
+                                    System.out.println("ingrese el nuevo nombre de la mascota");
+                                    String masc = sc.next();
+                                    equipos.get(posi).setMascota(masc);
+                                    break;
+                                default:
+                                    System.out.println("ingreso un valor invalido");
+                                    break;
+
+                            }
+                            break;
+                        case 3:
+                            System.out.println("eliminar equipo");
+                            for (int i = 0; i < equipos.size(); i++) {
+                                System.out.println(i + ")" + equipos.get(i).getNombre());
+                            }
+                            System.out.println("ingrese el numero del equipo que desea eliminar");
+                            int elim = sc.nextInt();
+                            equipos.remove(elim);
+                            break;
+                        case 4:
+                            for (int i = 0; i < equipos.size(); i++) {
+                                System.out.println(equipos.get(i) + "\n");
+                            }
+                            break;
+                        default:
+                            System.out.println("ingreso un valor invalido");
+                            break;
+
+                    }
                     break;
                 case 2:
                     int resp2 = -1;
@@ -99,7 +199,7 @@ public class Lab4 {
                                             }
                                         }
                                     }
-                                    int estrellasfinal=Integer.valueOf(estrellas);
+                                    int estrellasfinal = Integer.valueOf(estrellas);
 
                                     switch (respj) {
                                         case 1:
